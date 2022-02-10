437c7fb0df694acbbe3d39f178bfedd6caadf073
EMWD <imba2511@yandex.ru>
2022-02-10 17:44:56 +0300

    routs

--- mysite/botsite/urls.py
+++ mysite/botsite/urls.py
@@ -1,7 +1,9 @@
-from django.views.generic.edit import DeleteView
 from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.TestView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('features/', views.FeaturesView.as_view(), name='features'),
+    path('donate/', views.DonateView.as_view(), name='donate'),
+    path('commands/', views.CommandsView.as_view(), name='commands'),
 ]
--- mysite/botsite/views.py
+++ mysite/botsite/views.py
@@ -3,7 +3,6 @@ from django.views import View, generic
 from django.views.generic.base import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView, DeleteView
-from wsgiref.util import FileWrapper
 from django.urls import reverse_lazy
 # from .models import Post
 from icecream import ic
@@ -15,6 +14,21 @@ from django.conf import settings
 from .models import *
 
 
-class TestView(generic.ListView):
+class HomeView(generic.ListView):
     queryset = []
     template_name = 'index.html'
+
+
+class DonateView(generic.ListView):
+    queryset = []
+    template_name = 'donate.html'
+
+
+class FeaturesView(generic.ListView):
+    queryset = []
+    template_name = 'features.html'
+
+
+class CommandsView(generic.ListView):
+    queryset = []
+    template_name = 'commands.html'
